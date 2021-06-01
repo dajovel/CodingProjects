43289f20c5f248139bda50530423b1b1b7532b1d
david.jovel <david.jovel@DJovel.local>
2021-05-31 20:57:19 -0500

    Adding a new file to see if this works.

---
+++ CodingDay1.py
@@ -0,0 +1,43 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what you print')")
+print("Hello world!\nHello World!\nHello World!")
+#contatinate
+print("Hello" + " Angela")
+# to add a space we can add at end of "Hello " add at beginning of " Angela" or ("Hello" + " " + " "Angela")
+print("Hello" + " " + "Angels") #this one contains the space
+#spaces IndentationError: Dont add spaces in front of code.
+print("Day 1 - String Manipulation")
+print('String Concatination is done with the "+" sign.')
+print('e.g print("Hello "+ "world")')
+print("New lines can be created with a backslash and n.")
+#Input Function
+# input() will get user input in console
+# Then print() will print the word "hello" and the user input
+#print("Hello " + input("What is your name?"))
+#print(input("What is your name? ") + len(input))
+#print( len( input("What is your name? ")))
+#to comment out code "command + / "
+
+# import re
+# for test_string in ['555-1212', 'ILL-EGAL']:
+#     if re.match(r'^\d{3}-\d{4}$', test_string):
+#         print (test_string, 'is a valid US local phone number')
+#     else:
+#         print (test_string, 'rejected')
+
+# names = input('What is your name?\n')
+# #Names = the input of the answer What is your name
+# print ('Hi, %s.' % names)
+# #After we print Hi and the %s = name output output will be Hi "name input"
+
+
+# friends = ['john', 'pat', 'gary', 'michael']
+# for i, name in enumerate(friends):
+#     print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+height = "Jovel"
+    
+print(type(height))
+
+    
